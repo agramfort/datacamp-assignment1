7d4ee1689d8983e9a11b719afba04dbb7db11c6b
Mohamed Babana <mohamed@Mohameds-MacBook-Pro.local>
2020-11-10 00:36:11 +0100

    a

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,21 +1,27 @@
+# noqa: D100
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
+from sklearn.utils.multiclass import check_classification_targets
 from sklearn.utils.validation import check_array
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     """Write docstring
     """
-    def __init__(self):
+    def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
         """Write docstring
         """
         X, y = check_X_y(X, y)
+        check_classification_targets(y)
         self.classes_ = np.unique(y)
         # XXX fix
+        self.X_ = X
+        self.y_ = y
+
         return self
 
     def predict(self, X):
@@ -23,8 +29,13 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         """
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0], dtype = self.classes_.dtype)
         # XXX fix
+        for i in range(y_pred.shape[0]):
+            euclidean_distances = np.linalg.norm(self.X_ - X[i,:], axis = 1)
+            index_closest = np.argmin(euclidean_distances)
+            y_pred[i] = self.y_[index_closest]
+            
         return y_pred
 
     def score(self, X, y):
